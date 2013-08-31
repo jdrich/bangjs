64efd90bb8a126b8a75cbc0c4c902f9ab4b91fb1
Jonathan Rich <jdrich@gmail.com>
2013-08-31 11:24:43 -0400

    Writing some tests, cleaning up a few things, remembering that node has an assert module.

--- bang.js
+++ bang.js
@@ -1,104 +1,108 @@
-"use strict";
-
 var parse = function (data) {
-  data = data.split('');
+    data = data.split('');
 
-  var index = 0;
-  var length = data.length;
-  var segments = [];
-  var buffer = [];
-  var state = 'output';
+    var index = 0,
+        length = data.length,
+        segments = [],
+        buffer = [],
+        state = 'output';
 
-  while (index < length) {
-    var char = data[index];
+    while (index < length) {        
+        var curr = data[index];
 
-    switch (char) {
-      case 's':
-        var length = buffer.length;
+        switch (curr) {
+            case 's':
+                if (buffer.length < 3 || state !== 'output') {                    
+                    buffer.push(curr);
 
-        if (length < 3 || state !== 'output') {
-          buffer.push(char);
+                    break;
+                }
+                
+                var lt_index = buffer.length - 3,
+                    bang_index = buffer.length - 2,
+                    j_index = buffer.length - 1;
 
-          break;
+                var lt_check = buffer[lt_index] === '<',
+                    bang_check = buffer[bang_index] === '!',
+                    j_check = buffer[j_index] === 'j';
+
+                var is_bang = lt_check && bang_check && j_check;
+
+                if (is_bang === true) {
+                    state = 'code';
+
+                    buffer.pop();
+                    buffer.pop();
+                    buffer.pop();
+
+                    segments.push( buffer.join('') );
+
+                    segments.push( '<!js' );
+
+                    buffer = [];
+                } else {
+                    buffer.push(curr);
+                }
+
+                break;
+            case '>':
+                if (buffer.length < 1 || state !== 'code' ) {
+                    buffer.push(curr);
+
+                    break;
+                }
+
+                if (buffer[buffer.length - 1] === '!') {
+                    state = 'output';
+
+                    buffer.pop();
+
+                    segments.push( buffer.join('') );
+                    segments.push( '!>' );
+
+                    buffer = [];
+                } else {
+                    buffer.push( 's' );
+                }
+
+                break;
+            case '"':
+            case '\'':
+            case '\\':
+            case ':':
+            default:
+                buffer.push(curr);
+
+                break;
         }
 
-        var lt_index = length - 3,
-          bang_index = length - 2,
-          j_index = length - 1;
-
-        var lt_check = buffer[lt_index] === '<',
-          bang_check = buffer[bang_index] === '!',
-          j_check = buffer[j_index] === 'j';
-
-        var is_bang = lt_check && bang_check && j_check;
-
-        if (is_bang === true) {
-          state = 'code';
-
-          buffer.pop();
-          buffer.pop();
-          buffer.pop();
-
-          segments.push( buffer.join('') );
-
-          segments.push( '<!js' );
-
-          buffer = [];
-        } else {
-          buffer.push( 's' );
-        }
-
-        break;
-      case '>':
-        if (buffer.length < 1 || state !== 'code' ) {
-          buffer.push(char);
-
-          break;
-        }
-
-        if (buffer[buffer.length - 1] === '!') {
-          state = 'output';
-
-          buffer.pop();
-
-          segments.push( buffer.join('') );
-          segments.push( '!>' );
-
-          buffer = [];
-        } else {
-          buffer.push( 's' );
-        }
-
-        break;
-      case '"':
-      case '\'':
-      case '\\':
-      case ':':
-      default:
-        buffer.push(char);
-
-        break;
+        index++;
     }
 
-    index++;
-  }
+    if( state !== 'output') {
+        throw new Error( 'Finished parsing in an invalid state! Missing closing tag.' );
+    }
 
+    buffer.length && segments.push(buffer.join(''));
 
-  return segments;
+    return segments.length === 0 ? [''] : segments;
 };
 
-var get_body = function (data) {
-  var body = 'var __ret = \'\';';
+var getBody = function (data) {
+    var body = 'var __ret = \'\';';
 
-  data = parse(data);
+    data = parse(data);
 
-  console.log( data );
+    console.log( data );
 
-  body += 'return __ret;';
+    body += 'return __ret;';
 
-  return body;
+    return body;
 };
+
+module.exports.parse = parse;
 
 module.exports.compile = function ( data ) {
-  return new Function('__data', get_body(data));
+    return new Function('__data', get_body(data));
 };
+
--- tests/parser.js
+++ tests/parser.js
@@ -1,32 +1,58 @@
-"use strict";
+var assert = require( 'assert' );
 
-var tests = require( './tests' );
-
-tests.makeTest(
-  'Test empty strings returns no tokens',
-  '',
-  ['']
-);
-
-module.exports = tests;
-
-/*make_test(
-  'Testing empty template invocation',
-  '',
-  ''
-
-);
-
-make_test(
-  'Basic tag detection',
-  '<!js var three = 3; !>',
-  'var three = 3;',
-  null,
-  ''
-);
-
-make_test(
-  'Echo',
-  'hello<!js var three = 3; !>',
-  'var three = 3;'
-);*/
+module.exports.run = function( bang ) {
+    console.log('Running unit tests for bangjs parser');
+    
+    try {   
+        assert.deepEqual(
+            bang.parse(''),
+            [''],
+            'Empty string returns single empty token.'
+        );
+        
+        assert.deepEqual(
+            bang.parse('hello'),
+            ['hello'],
+            'Tagless string returns single token.'
+        );
+        
+        assert.deepEqual(
+            bang.parse('<strong>hello</strong>'),
+            ['<strong>hello</strong>'],
+            'Tagless HTML string returns single token.'
+        );
+        
+        assert.deepEqual(
+            bang.parse('hello!'),
+            ['hello!'],
+            'String with ! doesn\'t puke.'
+        );
+        
+        assert.deepEqual(
+            bang.parse('hello<!js world !>'),
+            ['hello', '<!js', ' world ', '!>'],
+            'Parser detects opening and closing tags.'
+        );
+        
+        assert.throws(
+            function () { bang.parse('hello<!js world ') },
+            Error,
+            'Parser detects exit in invalid state'
+        );
+        
+        assert.throws(
+            function () { bang.parse('hello<!js') },
+            Error,
+            'Parser detects exit in invalid state'
+        );
+        
+        
+        console.log('Remarkably, everything went okay!');
+    } catch (error) {
+        console.log( 'Test failure: ' + error.message );
+        console.log( 'Expected: ', error.expected );
+        console.log( '  Actual: ', error.actual );
+        
+        process.exit(1);
+    }
+};
