2239bd0df4fab2abd3888b68705530650e306b84
Jonathan <jdrich@gmail.com>
2013-08-30 20:25:40 -0400

    Stubbing out parser.

--- bang.js
+++ bang.js
@@ -1,15 +1,60 @@
 "use strict";
 
 var parse = function (data) {
+  data = data.split('');
 
+  var index = 0;
+  var length = data.length;
+  var segments = [];
+  var buffer = '';
+  var tag_buffer = '';
+  var state = 'output';
+
+  while (index < length) {
+    var char = data[index];
+
+    switch (char) {
+      case '!':
+      case 's':
+      case '"':
+      case '\'':
+      case '\\':
+      case ':':
+
+      case '>':
+        if ( state === 'code' && tag_buffer === '!') {
+          // Closing a ! tag.
+          buffer.pop();
+
+          segments.push(buffer.join(''));
+        }
+      case '<':
+        if ( state === 'output' && tag_buffer === '') {
+          tag_buffer = '<';
+        }
+      default:
+        buffer.push(char);
+
+        break;
+    }
+
+    index++;
+  }
+
+
+  return segments;
 };
 
 var get_body = function (data) {
-    var body = 'var __ret = \'\';';
+  var body = 'var __ret = \'\';';
 
-    body += 'return __ret;';
+  data = parse(data);
 
-    return body;
+  console.log( data );
+
+  body += 'return __ret;';
+
+  return body;
 };
 
 module.exports.compile = function ( data ) {
